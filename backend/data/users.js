687b59b56b8b01adcf85348e92d309b6d0482e1c
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-11-09 13:06:01 -0800

    installed bcryptjs

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        name: "Admin User",
+        email: "admin@gmail.com",
+        password: xxxx,
+        isAdmin: true
+    },
+    {
+        name: "John Doe",
+        email: "John@gmail.com",
+        password: xxxx,
+    },
+    {
+        name: "Jane Doe",
+        email: "Jane@gmail.com",
+        password: xxxx,
+    },
+]
