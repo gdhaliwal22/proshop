f0a32f563cf6bb87bcc967e99e339e26ba12fe6a
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-11-09 12:18:45 -0800

    refactor server to import from

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
 
 dotenv.config()
@@ -13,6 +13,7 @@ app.get('/', (req, res) => {
 app.get('/api/products', (req, res) => {
     res.json(products)
 })
+
 
 app.get('/api/products/:id', (req, res) => {
     const product = products.find(p => p._id === req.params.id)
