d004fc487a36d6a3c74f962143160cd1e7fd27a1
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-11-12 13:21:46 -0800

    updated users info

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,18 +1,22 @@
+import bcrypt from 'bcryptjs'
+
 const users = [
     {
         name: "Admin User",
         email: "admin@gmail.com",
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10),
         isAdmin: true
     },
     {
         name: "John Doe",
         email: "John@gmail.com",
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10),
     },
     {
         name: "Jane Doe",
         email: "Jane@gmail.com",
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10),
     },
 ]
+
+export default users
